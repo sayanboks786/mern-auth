37ed7ef4ab6dd7ada941889b08b09bdc812ea274
sayanboks786 <sayanboks786.com>
2025-05-27 20:57:27 +0530

    add userAuth middleware

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,11 +1,14 @@
 import express, { Router } from 'express';
 
-import { register, login, logout } from '../controllers/auth.controller.js';
+import { register, login, logout, sendVerifyOtp, verifyEmail } from '../controllers/auth.controller.js';
+import userAuth from '../middleware/userAuth.middleware.js';
 
 const authRoute = express.Router();
 
 authRoute.post("/register", register);
 authRoute.post("/login", login);
 authRoute.post("/logout", logout);
+authRoute.post("/send-verify-otp", userAuth, sendVerifyOtp);
+authRoute.post("/verify-account", userAuth, verifyEmail);
 
 export default authRoute;
